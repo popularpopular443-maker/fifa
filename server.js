1919c62a9b0e8fab46e98335ca7127362bf2c5a5
Dhileeban <popularpopular443@gmail.com>
2026-07-11 14:32:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 app.use(express.static('index.html'));
 
 const port = process.env.PORT || 8080;
-app.listen(port, '0.0.0.0', () => {
-  console.log("Running on port" + port);
+
+app.listen(port, '0.0.0.0', () => 
+{
+  console.log("App running on port" + port);
 });
